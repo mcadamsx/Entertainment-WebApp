df778fa5cfc385c32c84b2e24a47a70f53ae4e3a
Richard McAdams <93725119+mcthekreator@users.noreply.github.com>
2024-02-16 11:12:15 +0000

    update on series page

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,7 +2,7 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject, INJECTOR } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  // Check if sessionStorage is available
+
   if (typeof sessionStorage !== 'undefined' && sessionStorage.getItem('email')) {
     return true;
   } else {
