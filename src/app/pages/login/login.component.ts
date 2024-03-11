701f708664bce0e6d6a17e9da8676f26783cfdd0
Richard McAdams <93725119+mcadamsx@users.noreply.github.com>
2024-03-11 14:49:23 +0000

    added loading spinner

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,14 +5,16 @@ import {NgIf} from "@angular/common";
 import{MovieServiceService} from "../../services/movie-service.service";
 import {Router} from'@angular/router'
 import {ToastrService} from "ngx-toastr";
+import {SpinnerComponent} from "../../components/spinner/spinner.component";
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule, NgIf, NgIf],
+  imports: [RouterLink, ReactiveFormsModule, NgIf, NgIf, SpinnerComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  isLoading = false
   loginForm = this.fb.group({
     email: ["", [Validators.required, Validators.email]],
     password: ["", [Validators.required]]
@@ -25,14 +27,22 @@ export class LoginComponent {
     return this.loginForm.controls['password']
   }
   loginUser(){
+   this.isLoading = true
    const{ email,password} = this.loginForm.value;
+    setTimeout(()=>{
+      this.isLoading = false
+    }, 3000)
    this.authService.getUserByEmail(email as string).subscribe(
      response =>{
        if (response.length > 0 && response[0].password === password){
          sessionStorage.setItem('email', email as string)
+
            this.toaster.success('welcome back!', ' login successfully');
          this.router.navigate(['./home']);
        }else{
+         setTimeout(()=>{
+           this.isLoading = false
+         }, 2000)
          this.toaster.error('email or password invalid', ' Sorry');
        }
      },
