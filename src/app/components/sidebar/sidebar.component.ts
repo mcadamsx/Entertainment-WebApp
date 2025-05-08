8cc1dcaed55e020f6ea71a989a0bf77e024b7610
mcadamsx <richhard.mcadams@amalitech.com>
2025-05-08 12:20:11 +0000

    feat: implemented sidebar component

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css',
+})
+export class SidebarComponent {
+  constructor(private router: Router) {}
+
+  logout() {
+    this.router.navigate(['login']);
+  }
+
+}
