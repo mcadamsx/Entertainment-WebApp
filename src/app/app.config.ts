9e29b128adc197a776caa77700d602e08b15d08f
mcadamsx <richhard.mcadams@amalitech.com>
2025-05-08 12:12:17 +0000

    chore: (project setup): installed dependancies and packages

---
+++ src/app/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import {MovieServiceService} from "./services/movie-service.service";
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { provideToastr } from 'ngx-toastr';
+import {provideHttpClient, withFetch} from "@angular/common/http";
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes),  [provideHttpClient(withFetch())],[MovieServiceService],  provideAnimations(),
+    provideToastr() , provideClientHydration()]
+};
